2db008000d98e002dd6684270cad12763554e454
lyle <867708794@qq.com>
2016-08-16 11:26:48 +0800

    修改

--- src/utils/wx/WXBizMsgCrypt.java
+++ src/utils/wx/WXBizMsgCrypt.java
@@ -218,11 +218,9 @@ public class WXBizMsgCrypt {
 	 * 	<li>生成安全签名</li>
 	 * 	<li>将消息密文和安全签名打包成xml格式</li>
 	 * </ol>
-	 * 
 	 * @param replyMsg 公众平台待回复用户的消息，xml格式的字符串
 	 * @param timeStamp 时间戳，可以自己生成，也可以用URL参数的timestamp
 	 * @param nonce 随机串，可以自己生成，也可以用URL参数的nonce
-	 * 
 	 * @return 加密后的可以直接回复用户的密文，包括msg_signature, timestamp, nonce, encrypt的xml格式的字符串
 	 * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
 	 */
@@ -250,12 +248,10 @@ public class WXBizMsgCrypt {
 	 * 	<li>若验证通过，则提取xml中的加密消息</li>
 	 * 	<li>对消息进行解密</li>
 	 * </ol>
-	 * 
 	 * @param msgSignature 签名串，对应URL参数的msg_signature
 	 * @param timeStamp 时间戳，对应URL参数的timestamp
 	 * @param nonce 随机串，对应URL参数的nonce
 	 * @param postData 密文，对应POST请求的数据
-	 * 
 	 * @return 解密后的原文
 	 * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
 	 */
