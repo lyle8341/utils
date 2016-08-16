c13ae75b4f5c30529b617b2813ea3ac0cc3abaee
lyle <867708794@qq.com>
2016-08-16 11:44:02 +0800

    删除

--- src/constant/WXServerConfig.java
+++
@@ -1,8 +0,0 @@
-package constant;
-
-public class WXServerConfig {
-
-	public static final String TOKEN = "TEST";
-	public static final String APPID = "djdkfjk4654f6d6ddfdf";
-	public static final String ENCODINGAESKEY = "dfddfdfdFJKDJFKD";
-}
