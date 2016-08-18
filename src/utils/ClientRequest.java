451e98177e12be772839c588ab723a9f67a6b207
lyle <867708794@qq.com>
2016-08-18 14:02:59 +0800

    增加

---
+++ src/utils/ClientRequest.java
@@ -0,0 +1,48 @@
+package utils;
+
+import java.io.IOException;
+
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+
+/**
+ * commons-httpclient是一个遗留版本，现在官方已经不推荐使用了。
+ * @author Lyle
+ *
+ */
+public class ClientRequest {
+
+	//private static String url = "http://www.apache.org/";
+
+	public static String sendHttp(String url) throws IOException {
+
+		String response = null;
+		// Create an instance of HttpClient
+		HttpClient client = new HttpClient();
+		// Create a method instance.
+		GetMethod method = new GetMethod(url);
+		// Provide custom retry handler is necessary
+		method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+				new DefaultHttpMethodRetryHandler(3, false));
+		try {
+		      // Execute the method.
+		      int statusCode = client.executeMethod(method);
+
+		      if (statusCode != HttpStatus.SC_OK) {
+		        System.err.println("Method failed: " + method.getStatusLine());
+		      }
+		      // Read the response body.
+		      byte[] responseBody = method.getResponseBody();
+		      // Deal with the response.
+		      // Use caution: ensure correct character encoding and is not binary data
+		      response = new String(responseBody);
+		    }finally {
+		      // Release the connection.
+		      method.releaseConnection();
+		    }  
+		return response;
+	}
+}
